99e2281e8c029f92dac570412fb51385f6525521
Ahmed Alamin <137452474+ahmedalaminn@users.noreply.github.com>
2024-07-26 03:07:55 +0000

    Unit 9 Session 1 Problem Set Version 1 Solutions

--- main.py
+++ main.py
@@ -1,131 +1,137 @@
-class TreeNode():
-   def __init__(self, value, left=None, right=None):
-       self.val = value
-       self.left = left
-       self.right = right
-     
-# Problem 1
-def is_univalued(root):
-  if not root:
-    return False
+from collections import deque
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
     
-  root_val = root.val
-  
-  def dfs(root):
-    if not root:
-      return True
-    elif root.val != root_val:
+# Problem 1
+def is_symmetric(root):
+  if root is None:
+    return True
+    
+  queue = deque([(root.left, root.right)])
+  while queue:
+    left, right = queue.popleft()
+    if not left and not right:
+      continue
+    if not left or not right or left.val != right.val:
       return False
-    else:
-      return dfs(root.left) and dfs(root.right)
 
-  return dfs(root)
+    queue.append((left.left, right.right))
+    queue.append((left.right, right.left))
 
-root = TreeNode(5, TreeNode(5, TreeNode(5)), TreeNode(5))
-print(is_univalued(root))
+  return True
+
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.left.left = TreeNode(3)
+root.left.right = TreeNode(4)
+
+root.right = TreeNode(2)
+root.right.left = TreeNode(4)
+root.right.right = TreeNode(3)
+
+print(is_symmetric(root))
+#       1
+#     /   \
+#    /     \
+#   2       2
+#  / \     / \
+# 3   4   4   3
 
 # Problem 2
-def height(root):
-  if not root:
-    return 0
-  return 1 + max(height(root.left), height(root.right))
+def binary_tree_paths(root):
+  paths = []
 
-print(height(root))
+  def dfs_helper(node, path):
+    if node is not None:
+      path.append(node.val)
+      if not node.left and not node.right:
+        paths.append(list(path))
+      else:
+        dfs_helper(node.left, path)
+        dfs_helper(node.right, path)
+      path.pop()
+
+  dfs_helper(root, [])
+  return paths
+
+print(binary_tree_paths(root))
+
 
 # Problem 3
-class TreeNode():
-   def __init__(self, key, value, left=None, right=None):
-       self.key = key
-       self.val = value
-       self.left = left
-       self.right = right
+def min_diff_in_bst(root):
+  def dfs(root, values):
+    if not root:
+      return values
+    values.append(root.val)
+    dfs(root.right, values)
+    dfs(root.left, values)
+    
+  values = []
+  dfs(root, values)
+  
+  values = sorted(values)
+  min_diff = float('inf')
+  for i in range(len(values) - 1):
+      diff = abs(values[i + 1] - values[i])
+      if diff < min_diff:
+          min_diff = diff
+  return min_diff
 
-def insert(root, key, value):
-  if root:
-    if key > root.key:
-      if root.right:
-        return insert(root.right, key, value)
-      else:
-        root.right = TreeNode(key, value)
-    elif key < root.key:
-      if root.left:
-        return insert(root.left, key, value)
-      else:
-        root.left = TreeNode(key, value)
-    else:
-        root.val = value
-  return root
 
-root = TreeNode(10, 'Ahmed', TreeNode(9, 'Cat'), TreeNode(11, 'Dog'))
-insert(root, 10, 'Zhanna')
-print(root.val)
+root = TreeNode(1)
+root.left = TreeNode(0)
+root.right = TreeNode(48)
+root.right.left = TreeNode(12)
+root.right.right = TreeNode(49)
+print(min_diff_in_bst(root))
 
 # Problem 4
-def remove_bst(root, key):
-  
-  def inorder_successor(root):
-    curr = root
-    while curr.left: 
-      curr = curr.left
-    return curr
+def increasing_bst(root):
+  def inorder(node, values):
+      if not node:
+          return
+      inorder(node.left, values)
+      values.append(node.val)
+      inorder(node.right, values)
 
-  if root is None:
-    return root
-  elif key < root.key:
-    root.left = remove_bst(root.left, key)
-  elif key > root.key:
-    root.right = remove_best(root.right, key)
-  else:
-    # Case 1: Node with no children (leaf node)
-    if root.left is None and root.right is None:
-        return None
-    # Case 2: Node with only one child
-    elif root.left is None:
-        return root.right
-    elif root.right is None:
-        return root.left
-    # Case 3: Node with two children
-    else:
-        temp = inorder_successor(root.right)
-        root.key = temp.key
-        root.right = remove_bst(root.right, temp.key)
-      
-    return root
+  values = []
+  inorder(root, values)
+  
+  root = TreeNode(values[0])
+  curr = root
+
+  for val in values[1:]:
+      curr.right = TreeNode(val)
+      curr = curr.right
+  return root
+
+new_root = increasing_bst(root)
+
+curr = new_root
+while curr:
+  print(curr.val, end = ' ')
+  curr = curr.right
+print()
 
 # Problem 5
-def inorder_successor(root, current):
-  if current:
-    return None
-  elif current.right: # if node has right subtree
-    curr = current.right
-    while curr.left: 
-      curr = curr.left
-    return curr
-  else: # if node doesn't have right subtree
-    inorder_successor = None
-    curr = root
-    while curr != current:
-      if curr.key > current.key:
-        inorder_successor = curr
-        curr = curr.left
-      else:
-        curr = curr.right
-        
-    return curr
+def can_split(root):
+  def dfs_counter(root):
+    if root is None:
+      return 0
+    return 1 + dfs_counter(root.left) + dfs_counter(root.right)
 
-# Problem 6
-def merge_trees(self, root1, root2):
-  if not root1 and not root2:
-    return None
-  elif not root1: 
-    return root2
-  elif not root2: 
-    return root1
-  else:
-    merged = TreeNode(root1.val + root2.val)
-    merged.left = merge_trees(root1.left, root2.left)
-    merged.right = merge_trees(root2.right, root2.right)
-  return merged
-    
+  return dfs_counter(root.left) == dfs_counter(root.right)
+  
+root = TreeNode(1)
+root.left = TreeNode(0)
+root.left.right = TreeNode(2)
+root.right = TreeNode(48)
+root.right.right = TreeNode(49)
+print(can_split(root))
 
+  
     
